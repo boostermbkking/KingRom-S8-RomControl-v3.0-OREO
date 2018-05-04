f77ba318072d6f020850fc2f2f9e3190db1a1804
boostermbkking <kingdual@gmail.com>
2018-05-04 23:14:28 +0200

    v11

--- app/build/generated/source/r/debug/android/support/v4/R.java
+++ app/build/generated/source/r/debug/android/support/v4/R.java
@@ -53,18 +53,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f070099;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f0800ca;
-        public static final int notification_bg = 0x7f0800cb;
-        public static final int notification_bg_low = 0x7f0800cc;
-        public static final int notification_bg_low_normal = 0x7f0800cd;
-        public static final int notification_bg_low_pressed = 0x7f0800ce;
-        public static final int notification_bg_normal = 0x7f0800cf;
-        public static final int notification_bg_normal_pressed = 0x7f0800d0;
-        public static final int notification_icon_background = 0x7f0800d1;
-        public static final int notification_template_icon_bg = 0x7f0800d2;
-        public static final int notification_template_icon_low_bg = 0x7f0800d3;
-        public static final int notification_tile_bg = 0x7f0800d4;
-        public static final int notify_panel_notification_icon_bg = 0x7f0800d5;
+        public static final int notification_action_background = 0x7f0800cd;
+        public static final int notification_bg = 0x7f0800ce;
+        public static final int notification_bg_low = 0x7f0800cf;
+        public static final int notification_bg_low_normal = 0x7f0800d0;
+        public static final int notification_bg_low_pressed = 0x7f0800d1;
+        public static final int notification_bg_normal = 0x7f0800d2;
+        public static final int notification_bg_normal_pressed = 0x7f0800d3;
+        public static final int notification_icon_background = 0x7f0800d4;
+        public static final int notification_template_icon_bg = 0x7f0800d5;
+        public static final int notification_template_icon_low_bg = 0x7f0800d6;
+        public static final int notification_tile_bg = 0x7f0800d7;
+        public static final int notify_panel_notification_icon_bg = 0x7f0800d8;
     }
     public static final class id {
         public static final int action0 = 0x7f09000d;
@@ -120,7 +120,7 @@ public final class R {
         public static final int notification_template_part_time = 0x7f0b004b;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0f0151;
+        public static final int status_bar_notification_info_overflow = 0x7f0f0156;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f100104;
